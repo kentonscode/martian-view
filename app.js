c427eb6a22037acc3e22c0ab344355f932698713
kentonscode <kenton@kentonscode.com>
2015-10-19 10:08:13 -0700

    #1 added opportunity rover route

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 var curiosity = require('./server-routes/curiosity-route.js');
+var opportunity = require('./server-routes/opportunity-route.js')
 
 app.use('/curiosity-rover', curiosity);
+app.use('/opportunity-rover', opportunity);
 
 app.listen(1337);
