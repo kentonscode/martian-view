a9f50cf9a0a35b7a6648736a704968b066eb2c89
kentonscode <kenton@kentonscode.com>
2015-10-19 10:16:56 -0700

    #1 added spirit rover route and code to return images by date

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 var curiosity = require('./server-routes/curiosity-route.js');
-var opportunity = require('./server-routes/opportunity-route.js')
+var opportunity = require('./server-routes/opportunity-route.js');
+var spirit = require('./server-routes/spirit-route.js');
 
 app.use('/curiosity-rover', curiosity);
 app.use('/opportunity-rover', opportunity);
+app.use('/spirit-rover', spirit);
 
 app.listen(1337);
