fd3abd854f70a9ac3762266278d7ad5600406770
kentonscode <kentonscode@gmail.com>
2015-10-21 15:10:19 -0700

    updating error for routes

--- server-routes/curiosity-route.js
+++ server-routes/curiosity-route.js
@@ -3,14 +3,19 @@ var requestHttp = require('request');
 var curiosity = express.Router(); 
 
 curiosity.get('/:date', function(req, res, next) {
-  requestHttp('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=' + req.params.date + '&api_key=XXXXXXXXXXXXXXXXXXXXXXXXX',
-    function(error, response, body) {
-      var photos = JSON.parse(body).photos;
-      var img_src = {};
-      for (var i = 0; i < photos.length; i++) {
-        img_src[i] = photos[i].img_src;
+  requestHttp('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=' + req.params.date + '&camera=fhaz&api_key=',
+    function(errors, response, body) {
+      if (JSON.parse(body).errors == 'No Photos Found') {
+        res.send('No Photos Found For This Day');
+        console.log('no photos')
+      } else {
+          var photos = JSON.parse(body).photos;
+          var img_src = {};
+          for (var i = 0; i < photos.length; i++) {
+            img_src[i] = photos[i].img_src;
+          }
+        res.send(img_src);
       }
-      res.send(img_src);
     });
 });
 
