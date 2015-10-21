0a4c9749ed9e64c53345d08224f7b77b34dcfe7a
kentonscode <kentonscode@gmail.com>
2015-10-21 10:15:55 -0700

    working on if else for no photo error

--- server-routes/spirit-route.js
+++ server-routes/spirit-route.js
@@ -3,14 +3,18 @@ var requestHttp = require('request');
 var spirit = express.Router(); 
 
 spirit.get('/:date', function(req, res, next) {
-  requestHttp('https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?earth_date=' + req.params.date + '&api_key=XXXXXXXXXXXXXXXXXXXXXX',
+  requestHttp('https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?earth_date=' + req.params.date + '&api_key=XXXXXXXXXXXXXXXXXXXXXXXXXXX',
     function(error, response, body) {
-      var photos = JSON.parse(body).photos;
-      var img_src = {};
-      for (var i = 0; i < photos.length; i++) {
-        img_src[i] = photos[i].img_src;
+      if (JSON.parse(body).error == 'No Photos Found') {
+        res.send('No Photos Found For This Day');
+      } else {
+          var photos = JSON.parse(body).photos;
+          var img_src = {};
+          for (var i = 0; i < photos.length; i++) {
+            img_src[i] = photos[i].img_src;
+          }
+        res.send(img_src);
       }
-      res.send(img_src);
     });
 });
 
