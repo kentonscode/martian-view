67215e55c35b05c7d6715a18006430f6fcda8b88
kentonscode <kentonscode@gmail.com>
2015-10-22 11:37:18 -0700

    wokring on mongodb route

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ var curiosity = require('./server-routes/curiosity-route.js');
 var opportunity = require('./server-routes/opportunity-route.js');
 var spirit = require('./server-routes/spirit-route.js');
 var roversRoute = require ('./server-routes/rovers-route.js')
+var mongoose = require('mongoose');
+
+//mongodb
+
+mongoose.connect('mongodb://localhost/martian')
+
+mongoose.model('users', {name: String});
+
+app.get('/users', function(req, res) {
+  mongoose.model('users').find(function(err, users) {
+    res.send(users)
+  });
+});
+
 
 app.use('/curiosity-rover', curiosity);
 app.use('/opportunity-rover', opportunity);
